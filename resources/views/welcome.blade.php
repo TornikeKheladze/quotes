4e8d94a45b4331022dda50ca99c61ab5117cbc03
tornikeKheladze <xeladze.tornike@gmail.com>
2022-11-09 15:57:45 +0400

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+<!doctype html>
+
+<title>Movie Quotes</title>
+@vite('resources/css/app.css')
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+
+<body style="font-family: Open Sans, sans-serif">
+    <h1 class="bg-blue-500">MOVIE</h1>
+</body>
