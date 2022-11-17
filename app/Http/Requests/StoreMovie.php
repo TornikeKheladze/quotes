1a4817785f12f0a3003d81dbb7194e83775f7a65
tornikeKheladze <xeladze.tornike@gmail.com>
2022-11-17 16:47:26 +0400

    docs:update file

--- app/Http/Requests/StoreMovie.php
+++ app/Http/Requests/StoreMovie.php
@@ -6,11 +6,6 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StoreMovie extends FormRequest
 {
-	public function authorize()
-	{
-		return true;
-	}
-
 	public function rules()
 	{
 		return [
